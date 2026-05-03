a961ef40a1fa3ebfea458d0b1e5c6bc808b29cb3
gmkdigitalmedia <gmkdigitalmeda@gmail.com>
2026-05-03 17:26:24 +0900

    Fix production validation pipeline

--- scripts/aws/provision-staging.ts
+++ scripts/aws/provision-staging.ts
@@ -4,7 +4,6 @@ import {
   AssociateAddressCommand,
   AuthorizeSecurityGroupIngressCommand,
   CreateSecurityGroupCommand,
-  CreateTagsCommand,
   DescribeAddressesCommand,
   DescribeImagesCommand,
   DescribeInstancesCommand,
